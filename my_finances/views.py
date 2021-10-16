07fe83f30458bc0897d7186d20d4b9799bc49bfd
Rony Morales <65436006+Ronyaco@users.noreply.github.com>
2021-10-17 09:45:36 +1100

    views list and create income

--- my_finances/views.py
+++ my_finances/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from my_finances.models import Income
+
+class IncomeListView(ListView):
+    model = Income
+    paginate_by = 100
+
+
+class IncomeCreateView(CreateView):
+    model = Income
+    fields = ['name','value','date','category','repetitive','repetition_interval','repetition_time','details','user_id']
+    success_url = reverse_lazy('my_finances:income_list')
+
